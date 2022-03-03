b267da67543a3e3b20d71a3abb48141601b5c1ce
Ishan Vyas <ishan@deepsource.io>
2022-03-03 16:32:52 +0530

    Add valid code (#6)
    Signed-off-by: Ishan Vyas <ishan@deepsource.io>

--- src/PromotedProperties/User.php
+++ src/PromotedProperties/User.php
@@ -4,6 +4,11 @@ namespace App\PromotedProperties;
 
 class User extends AbstractUser
 {
+    // valid
+    public function __construct(public string $firstName, public string $lastName)
+    {
+    }
+
     // invalid: cannot declare promoted property outside a constructor
     public function setName(public string $firstName, public string $lastName): void
     {
